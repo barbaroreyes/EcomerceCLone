0e47704ea03b240fef4678279cf362729e36c132
barbaro reyes <barbaroreyes@barbaros-iMac.local>
2020-08-20 03:29:03 -0500

    organize a home page  small componet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-import  HomePage from './Componets/homePage'
+import  HomePage from './pages/Homepages/homePage'
+
+
 
 function App () {
   return <div>
     <HomePage/>
+    
+    
   </div>
 }
 
---
+++ src/pages/Homepages/homePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './home.styles.scss'
+import  Directory  from '../../Componets/Directory/Directory.Com'
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory/>
+  </div>
+);
+
+export default HomePage;
